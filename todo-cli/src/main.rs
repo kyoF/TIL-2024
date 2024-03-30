9859b103c4bbd72089f5be46c87ec0b550bae0e4
kyoF <fujiki.kyosuke@gmail.com>
2024-03-30 15:11:11 +0900

    fix: todo cli

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -1,27 +1,28 @@
-use std::{collections::HashMap, io::Read, str::FromStr};
+use std::collections::HashMap;
 
 struct Todo {
     map: HashMap<String, bool>,
 }
 
 impl Todo {
-    fn new() -> Result<Todo, std::io::Error> {
-        let mut f = std::fs::OpenOptions::new()
-            .write(true)
-            .create(true)
-            .read(true)
-            .open("db.txt")?;
-        let mut content = String::new();
-        f.read_to_string(&mut content)?;
-        let map: HashMap<String, bool> = content
-            .lines()
-            .map(|line| line.splitn(2, '\t').collect::<Vec<&str>>())
-            .map(|v| (v[0], v[1]))
-            .map(|(k, v)| (String::from(k), bool::from_str(v).unwrap()))
-            .collect();
-        Ok(Todo { map })
-    }
-    // use for loop
+    // // with text file
+    // fn new() -> Result<Todo, std::io::Error> {
+    //     let mut f = std::fs::OpenOptions::new()
+    //         .write(true)
+    //         .create(true)
+    //         .read(true)
+    //         .open("db.txt")?;
+    //     let mut content = String::new();
+    //     f.read_to_string(&mut content)?;
+    //     let map: HashMap<String, bool> = content
+    //         .lines()
+    //         .map(|line| line.splitn(2, '\t').collect::<Vec<&str>>())
+    //         .map(|v| (v[0], v[1]))
+    //         .map(|(k, v)| (String::from(k), bool::from_str(v).unwrap()))
+    //         .collect();
+    //     Ok(Todo { map })
+    // }
+    // // use for loop with text file
     // fn new() -> Result<Todo, std::io::Error> {
     //     let mut f = std::fs::OpenOptions::new()
     //         .write(true)
@@ -38,16 +39,40 @@ impl Todo {
     //         map.insert(String::from(key), bool::from_str(val).unwrap());
     //     }
     // }
+    fn new() -> Result<Todo, std::io::Error> {
+        let f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .read(true)
+            .open("db.json")?;
+        match serde_json::from_reader(f) {
+            Ok(map) => Ok(Todo { map }),
+            Err(e) if e.is_eof() => Ok(Todo {
+                map: HashMap::new(),
+            }),
+            Err(e) => panic!("An error occurred: {}", e),
+        }
+    }
     fn insert(&mut self, key: String) {
         self.map.insert(key, true);
     }
-    fn save(self) -> Result<(), std::io::Error> {
-        let mut content = String::new();
-        for (k, v) in self.map {
-            let record = format!("{}\t{}\n", k, v);
-            content.push_str(&record)
-        }
-        std::fs::write("db.txt", content)
+    // // with text file
+    // fn save(self) -> Result<(), std::io::Error> {
+    //     let mut content = String::new();
+    //     for (k, v) in self.map {
+    //         let record = format!("{}\t{}\n", k, v);
+    //         content.push_str(&record)
+    //     }
+    //     std::fs::write("db.txt", content)
+    // }
+    // with json file
+    fn save(self) -> Result<(), Box<dyn std::error::Error>> {
+        let f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open("db.json")?;
+        serde_json::to_writer_pretty(f, &self.map)?;
+        Ok(())
     }
     fn complete(&mut self, key: &String) -> Option<()> {
         match self.map.get_mut(key) {
