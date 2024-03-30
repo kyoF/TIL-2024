f7ded51abe06647d6f0e8f8195446e678f0184ac
kyoF <fujiki.kyosuke@gmail.com>
2024-03-30 12:05:00 +0900

    fix: todo cli (in progress)

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -1,10 +1,43 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, io::Read, str::FromStr};
 
 struct Todo {
     map: HashMap<String, bool>,
 }
 
 impl Todo {
+    fn new() -> Result<Todo, std::io::Error> {
+        let mut f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .read(true)
+            .open("db.txt")?;
+        let mut content = String::new();
+        f.read_to_string(&mut content)?;
+        let map: HashMap<String, bool> = content
+            .lines()
+            .map(|line| line.splitn(2, '\t').collect::<Vec<&str>>())
+            .map(|v| (v[0], v[1]))
+            .map(|(k, v)| (String::from(k), bool::from_str(v).unwrap()))
+            .collect();
+        Ok(Todo { map })
+    }
+    // use for loop
+    // fn new() -> Result<Todo, std::io::Error> {
+    //     let mut f = std::fs::OpenOptions::new()
+    //         .write(true)
+    //         .create(true)
+    //         .read(true)
+    //         .open("db.txt")?;
+    //     let mut content = String::new();
+    //     f.read_to_string(&mut content)?;
+    //     let mut map = HashMap::new();
+    //     for entries in content.lines() {
+    //         let mut values = entries.split('\t');
+    //         let key = values.next().expect("No key");
+    //         let val = values.next().expect("No value");
+    //         map.insert(String::from(key), bool::from_str(val).unwrap());
+    //     }
+    // }
     fn insert(&mut self, key: String) {
         self.map.insert(key, true);
     }
@@ -16,21 +49,33 @@ impl Todo {
         }
         std::fs::write("db.txt", content)
     }
+    fn complete(&mut self, key: &String) -> Option<()> {
+        match self.map.get_mut(key) {
+            Some(v) => Some(*v = false),
+            None => None,
+        }
+    }
 }
 
 fn main() {
     let action = std::env::args().nth(1).expect("Please specify an action");
     let item = std::env::args().nth(2).expect("Please specify an item");
 
-    let mut todo = Todo {
-        map: HashMap::new(),
-    };
+    let mut todo = Todo::new().expect("Initialisation of todo failed");
 
     if action == "add" {
         todo.insert(item);
         match todo.save() {
             Ok(_) => println!("todo saved"),
             Err(why) => println!("An error occurred: {}", why),
+        }
+    } else if action == "complete" {
+        match todo.complete(&item) {
+            None => println!("'{}' is not present in the list", item),
+            Some(_) => match todo.save() {
+                Ok(_) => println!("todo saved"),
+                Err(why) => println!("An error occurred: {}", why),
+            },
         }
     }
 }
