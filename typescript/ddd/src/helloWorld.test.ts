f2d57aee0175f73d8271c634a508f03a28380174
kyoF <fujiki.kyosuke@gmail.com>
2024-04-16 21:29:25 +0900

    del: test file

--- typescript/ddd/src/helloWorld.test.ts
+++
@@ -1,5 +0,0 @@
-import { helloWorld } from './helloWorld';
-
-test('say-hello-world', () => {
-  expect(helloWorld('world')).toBe('Hello world!');
-});
--- typescript/ddd/src/helloWorld.ts
+++
@@ -1,7 +0,0 @@
-export const helloWorld = (name: string): string => {
-  const res = `Hello ${name}!`;
-  console.log(res);
-  return res;
-}
-
-helloWorld('world');
