54cfa1a377b912ff5ebe877b18f2241e3d32eef5
kyoF <fujiki.kyosuke@gmail.com>
2024-04-21 15:27:34 +0900

    fix: use repository at domain service

--- typescript/ddd/src/Domain/service/BookISBNDuplicationCheckDomainService/ISBNDuplicationCheckDomainService.ts
+++
@@ -1,8 +0,0 @@
-import { BookId } from 'Domain/models/Book/BookId/BookId';
-
-export class ISBNDuplicationCheckDomainService {
-  async execute(_ /* bookId */: BookId): Promise<boolean> {
-    const isDuplicateISBN = false;
-    return isDuplicateISBN;
-  }
-}
