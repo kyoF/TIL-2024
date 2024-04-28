30423f808eb222f9e524c70161d750dae7afa007
kyoF <fujiki.kyosuke@gmail.com>
2024-04-28 16:48:55 +0900

    fix: root page

--- remix/tutorial/app/root.tsx
+++ remix/tutorial/app/root.tsx
@@ -1,20 +1,32 @@
+import { json } from "@remix-run/node";
 import type { LinksFunction } from "@remix-run/node";
 import {
   Form,
+  Link,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
+
+import { getContacts } from "./data";
 
 import appStyleHref from "./app.css?url";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStyleHref },
-]
+];
+
+export const loader = async () => {
+  const contacts = await getContacts();
+  return json({ contacts });
+};
 
 export default function App() {
+  const { contacts } = useLoaderData<typeof loader>();
+
   return (
     <html lang="en">
       <head>
@@ -42,12 +54,37 @@ export default function App() {
             </Form>
           </div>
           <nav>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i></i>
+                      )}{" "}
+                      {contact.favorite ? (
+                        <span>*</span>
+                      ) : null}
+                    </Link>
+                  </li>
+                )
+                )}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
             <ul>
               <li>
-                <a href={`/contacts/1`}>Your Name</a>
+                <Link to={`/contacts/1`}>Your Name</Link>
               </li>
               <li>
-                <a href={`/contacts/2`}>Your Friend</a>
+                <Link to={`/contacts/2`}>Your Friend</Link>
               </li>
             </ul>
           </nav>
