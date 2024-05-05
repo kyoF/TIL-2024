8e503876d21a02cb2339938f9d5e99906834f9ca
kyoF <fujiki.kyosuke@gmail.com>
2024-05-05 16:30:31 +0900

    add: dto reqest type and response type

--- go/dddWithJWT/app/src/application/dto/auth.go
+++ go/dddWithJWT/app/src/application/dto/auth.go
@@ -1,7 +1,23 @@
 package dto
 
-type Response struct {
+type AuthRequest struct {
+	Username string `json:"username" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8"`
+}
+
+type AuthResponse struct {
 	ID       string
 	Username string
 	Email    string
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8"`
+}
+
+type LoginResponse struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
 }
