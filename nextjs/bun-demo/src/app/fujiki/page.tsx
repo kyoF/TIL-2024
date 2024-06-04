c423ef538abdd6d7bc635c7a0d93c6c499c3d6ca
kyoF <fujiki.kyosuke@gmail.com>
2024-06-04 19:05:41 +0900

    add: sample page

---
+++ nextjs/bun-demo/src/app/fujiki/page.tsx
@@ -0,0 +1,5 @@
+const Fujiki = () => {
+  return <><div><p>aaa</p></div><div><p>aaa</p></div><div><p>aaa</p></div><div><p>aaa</p></div><div><p>aaa</p></div><div><p>aaa</p></div><div><p>aaa</p></div><div><p>aaa</p></div></>
+}
+
+export default Fujiki;
---
+++ nextjs/bun-demo/src/app/test/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+const Fujiki = () => {
+  return (
+    <>
+      <div>fujiki</div>
+      <div>test</div>
+      <div>aaa</div>
+      <div>bbb</div>
+    </>
+  );
+}
+
+export default Fujiki;
