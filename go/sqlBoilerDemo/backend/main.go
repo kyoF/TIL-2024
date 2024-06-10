48c43eb7765f48b76ed3beeb8fb4c671e773ddc0
kyoF <fujiki.kyosuke@gmail.com>
2024-06-10 19:10:58 +0900

    add: infra layer and interface layer and main.go

--- go/sqlBoilerDemo/backend/main.go
+++ go/sqlBoilerDemo/backend/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"backend/presentation"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/user", presentation.GetUser())
+}
---
+++ go/sqlBoilerDemo/backend/presentation/router.go
@@ -0,0 +1,9 @@
+package presentation
+
+import "github.com/labstack/echo/v4"
+
+func GetUser() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return nil
+	}
+}
