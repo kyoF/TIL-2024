73647a7b93791db7ada7e598154fefcb5fc87d5f
kyoF <fujiki.kyosuke@gmail.com>
2024-06-17 21:42:13 +0900

    study: gorutine

--- go/goroutineDemo/main.go
+++ go/goroutineDemo/main.go
@@ -1,30 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
-
-func getLuckyNum(c chan<- int) {
-	fmt.Println("...")
-
-	rand.Seed(time.Now().Unix())
-	time.Sleep(time.Duration(rand.Intn(3000)) * time.Millisecond)
-
-	num := rand.Intn(10)
-	c <- num
-}
+import "fmt"
 
 func main() {
-	fmt.Println("What is today's lucky number?")
-
-	c := make(chan int)
-	go getLuckyNum(c)
-
-	num := <-c
-
-	fmt.Printf("Today's your lucky number is %d!\n", num)
-
-	close(c)
+	for i := 0; i < 3; i++ {
+		go func(i int) {
+			fmt.Println(i)
+		}(i)
+	}
 }
