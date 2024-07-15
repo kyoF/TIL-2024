837ee073c288d7d04f6b316ab8d1fd42c37cedfa
kyoF <fujiki.kyosuke@gmail.com>
2024-07-15 15:08:57 +0900

    del: mysql and redis dir

--- redis-demo/mysql/initdb.d/sql.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `users`
-(
-  id bigint auto_increment,
-  name varchar(255) NOT NULL,
-  password varchar(255),
-  PRIMARY KEY (`id`)
-);
-
-INSERT INTO users (`name`) VALUES ('山田太郎', 'yamadapassword'), ('鈴木花子', 'suzukipassword');
