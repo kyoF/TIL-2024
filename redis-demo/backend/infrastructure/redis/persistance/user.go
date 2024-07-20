b0835f14657b97a7613a5c306231b9bd1402da22
kyoF <fujiki.kyosuke@gmail.com>
2024-07-20 13:59:30 +0900

    add: redis persistance

---
+++ redis-demo/backend/infrastructure/redis/persistance/user.go
@@ -0,0 +1,30 @@
+package redispersistance
+
+import (
+	"context"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+
+	"backend/domain/repository"
+)
+
+type infra struct {
+	db *redis.Client
+}
+
+func NewRedisPersistance(db *redis.Client) repository.ISessionRepository {
+	return &infra{db: db}
+}
+
+func (i *infra) Set(sessionId, name string, deadline time.Duration) error {
+	ctx := context.Background()
+	err := i.db.Set(ctx, sessionId, name, deadline).Err()
+	return err
+}
+
+func (i *infra) Delete(sessionId string) error {
+	ctx := context.Background()
+	err := i.db.Del(ctx, sessionId).Err()
+	return err
+}
