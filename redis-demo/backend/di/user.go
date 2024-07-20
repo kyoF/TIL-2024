869015ea98486ede12a876f36cde74009070e1fc
kyoF <fujiki.kyosuke@gmail.com>
2024-07-20 15:50:14 +0900

    fix: middleware di for user

--- redis-demo/backend/di/user.go
+++ redis-demo/backend/di/user.go
@@ -5,17 +5,28 @@ import (
 
 	"github.com/go-redis/redis/v8"
 
+	"backend/application/queryservice"
 	"backend/application/usecase"
-	"backend/handler"
+	"backend/handler/middleware"
+	"backend/handler/router"
 	mysqlpersistance "backend/infrastructure/mysql/persistance"
 	redispersistance "backend/infrastructure/redis/persistance"
 )
 
-func InjectDependencies(mysql *sql.DB, redis *redis.Client) handler.IUserHandler {
-	return handler.NewUserHandler(
-		usecase.NewUserUsecase(
-			mysqlpersistance.NewMySQLPersistance(mysql),
-			redispersistance.NewRedisPersistance(redis),
+func InjectDependencies(mysql *sql.DB, redis *redis.Client) (
+	router.IUserRouter,
+	middleware.IMiddleware,
+) {
+	return router.NewUserRouter(
+			usecase.NewUserUsecase(
+				mysqlpersistance.NewMySQLPersistance(mysql),
+				redispersistance.NewRedisPersistance(redis),
+			),
 		),
-	)
+		middleware.NewMiddleware(
+			queryservice.NewAuthQueryService(
+				redispersistance.NewRedisPersistance(redis),
+			),
+		)
+
 }
