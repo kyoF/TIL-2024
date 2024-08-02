bd4fad01619b89d0fc88f977d59acc1cd9071015
kyoF <fujiki.kyosuke@gmail.com>
2024-08-02 18:44:36 +0900

    add: ent migrate script

--- go/ent-demo/backend/cmd/migration/main.go
+++ go/ent-demo/backend/cmd/migration/main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+	"backend/ent/migrate"
+	"backend/utils"
 	"context"
 	"fmt"
+	"log"
+	"os"
 
 	atlas "ariga.io/atlas/sql/migrate"
+	"entgo.io/ent/dialect"
+	"entgo.io/ent/dialect/sql/schema"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-    dataSourceName := fmt.Println("mysql://%s:%s/%s?charset=utf8&parseTime=True", "", "", "", "")
+	dataSourceName := fmt.Sprintf("mysql://%s:%s@%s:%s/%s?charset=utf8&parseTime=True",
+		utils.GetEnv("DATABASE_USER", "mysqluser"),
+		utils.GetEnv("DATABASE_PASSWORD", "mysqlpass"),
+		utils.GetEnv("DATABASE_HOST", "backend"),
+		utils.GetEnv("DATABASE_PORT", "3306"),
+		utils.GetEnv("DATABASE_NAME", "entdemo"),
+	)
 
-    ctx := context.Background()
+	ctx := context.Background()
 
+	dir, err := atlas.NewLocalDir("ent/migrate/migrations")
+	if err != nil {
+		log.Fatalf("failed creating atlas migration directory: %v", err)
+	}
+
+	opts := []schema.MigrateOption{
+		schema.WithDir(dir),
+		schema.WithMigrationMode(schema.ModeInspect),
+		schema.WithDialect(dialect.MySQL),
+		schema.WithFormatter(atlas.DefaultFormatter),
+	}
+
+	err = migrate.NamedDiff(ctx, dataSourceName, os.Args[1], opts...)
+	if err != nil {
+		log.Fatalf("failed generating migration file: %v", err)
+	}
 }
--- go/ent-demo/backend/go.mod
+++ go/ent-demo/backend/go.mod
@@ -5,11 +5,13 @@ go 1.22.1
 require (
 	ariga.io/atlas v0.19.1-0.20240203083654-5948b60a8e43
 	entgo.io/ent v0.14.0
+	github.com/go-sql-driver/mysql v1.8.1
 	github.com/google/uuid v1.3.0
 	github.com/labstack/echo/v4 v4.12.0
 )
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/agext/levenshtein v1.2.1 // indirect
 	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
 	github.com/go-openapi/inflect v0.19.0 // indirect
