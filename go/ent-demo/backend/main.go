7e5bf82af476b495622a204d9dcf4a4abff7f9be
kyof <fujiki.kyosuke@gmail.com>
2024-08-03 21:27:36 +0900

    add: main.go

--- go/ent-demo/backend/main.go
+++ go/ent-demo/backend/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/labstack/echo/v4"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-    e := echo.New()
+	e := echo.New()
+
+	e.GET("/", helloWorld)
+
+	e.Logger.Fatal(e.Start(":8888"))
+}
+
+// helloWorld ハンドラー
+func helloWorld(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
