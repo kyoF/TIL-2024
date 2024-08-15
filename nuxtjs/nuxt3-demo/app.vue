7a07aa67a39220d24b68a287a4bd5a7c757e251e
kyof <fujiki.kyosuke@gmail.com>
2024-08-15 16:28:25 +0900

    study: about view

--- nuxtjs/nuxt3-demo/app.vue
+++ nuxtjs/nuxt3-demo/app.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtPage />
+    <NuxtLayout>
+      <NuxtRouteAnnouncer />
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
+<script setup>
+useHead(
+  {
+    title: "Nuxt 3 Store",
+    link: [
+      { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" }
+    ]
+  }
+);
+</script>
