e9552184eeab8ecf8d44af1d303dd492280e978d
kyof <fujiki.kyosuke@gmail.com>
2024-08-15 16:38:46 +0900

    study: about async

--- nuxtjs/nuxt3-demo/pages/index.vue
+++ nuxtjs/nuxt3-demo/pages/index.vue
@@ -6,5 +6,17 @@
       <li><NuxtLink to="/price">price</NuxtLink></li>
       <li><NuxtLink to="/users">users</NuxtLink></li>
     </ul>
+    <div>
+      <ul>
+        <li v-for="user in users" :key="user.id">{{ user.id }}: {{ user.name }}</li>
+      </ul>
+    </div>
   </div>
 </template>
+<script setup>
+  const { data: users, error } = await useFetch('https://jsonplaceholder.typicode.com/userssss')
+  // console.log(error.value)
+  if(error.value){
+    throw createError({ statusCode: '404', statusMessage: 'Page Not Found!'})
+  }
+</script>
