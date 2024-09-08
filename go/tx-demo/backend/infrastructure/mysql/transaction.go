4ab961d433392df75b36bbd1acb900cb038b27ca
kyof <fujiki.kyosuke@gmail.com>
2024-09-08 18:25:21 +0900

    rename: tx -> txDB

--- go/tx-demo/backend/infrastructure/mysql/transaction.go
+++ go/tx-demo/backend/infrastructure/mysql/transaction.go
@@ -3,13 +3,13 @@ package mysqlinfra
 import "gorm.io/gorm"
 
 type transaction struct {
-	db *gorm.DB
-	tx *gorm.DB
+	db   *gorm.DB
+	txDB *gorm.DB
 }
 
 func (t *transaction) Transaction(f func() error) error {
 	return t.db.Transaction(func(tx *gorm.DB) error {
-		t.tx = tx
+		t.txDB = tx
 		return f()
 	})
 }
